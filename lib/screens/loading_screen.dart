6a126e8e3aa8aaf604c11556c4e004fe053c65b4
Jordan Gillard <jordan-gillard@outlook.com>
2020-04-25 15:06:29 +0200

    feat: Add loading screen to loading_screen.dart

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,9 +1,7 @@
-import 'dart:convert';
-
 import 'package:clima/services/location.dart';
 import 'package:clima/services/networking.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 var openWeatherMapApiKey = 'e542f831aca4693ae9887bddb0efe71d';
 
@@ -13,7 +11,6 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
-
   void getLocationData() async {
     Location location = Location();
     await location.getCurrentLocation();
@@ -33,6 +30,13 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: SpinKitCubeGrid(
+          color: Colors.amberAccent,
+          size: 100.0,
+        ),
+      ),
+    );
   }
 }
