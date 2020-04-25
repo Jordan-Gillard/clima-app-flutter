472a6e74fd277036f95400204573e5c83cf75e48
Jordan Gillard <jordan-gillard@outlook.com>
2020-04-25 09:05:37 +0200

    feat: Add getData method which gets weather data from weather API

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,6 @@
 import 'package:clima/services/location.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -7,13 +8,12 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
-
   void getLocation() async {
     Location location = Location();
     await location.getLocation();
     print(location.latitude);
     print(location.longitude);
-}
+  }
 
   @override
   void initState() {
@@ -21,9 +21,19 @@ class _LoadingScreenState extends State<LoadingScreen> {
     getLocation();
   }
 
+  void getData() async {
+    http.Response response = await http.get(
+        'http://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=439d4b804bc8187953eb36d2a8c26a02');
+    if (response.statusCode==200) {
+      print(response.body);
+    } else {
+      print(response.statusCode);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    getData();
     return Scaffold();
-
   }
 }
