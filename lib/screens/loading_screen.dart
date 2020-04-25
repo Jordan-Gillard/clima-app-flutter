8654be3719a6354c06760b60f74c99a06d934cf1
Jordan Gillard <jordan-gillard@outlook.com>
2020-04-25 15:01:00 +0200

    refactor: Improvements to code quality and api calls

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,8 +1,11 @@
 import 'dart:convert';
 
 import 'package:clima/services/location.dart';
+import 'package:clima/services/networking.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+
+var openWeatherMapApiKey = 'e542f831aca4693ae9887bddb0efe71d';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -10,38 +13,26 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
-  void getLocation() async {
+
+  void getLocationData() async {
     Location location = Location();
-    await location.getLocation();
-    print(location.latitude);
-    print(location.longitude);
+    await location.getCurrentLocation();
+    var latitude = location.latitude;
+    var longitude = location.longitude;
+    var url =
+        'http://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$openWeatherMapApiKey';
+    NetworkHelper networkHelper = NetworkHelper(url);
+    var weatherData = await networkHelper.getData();
   }
 
   @override
   void initState() {
     super.initState();
-    getLocation();
-  }
-
-  void getData() async {
-    http.Response response = await http.get(
-        'http://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=439d4b804bc8187953eb36d2a8c26a02');
-    if (response.statusCode==200) {
-      var jsonBody = jsonDecode(response.body);
-      var temperature = jsonBody['main']['temp'];
-      var city = jsonBody['name'];
-      var weatherId = jsonBody['weather'][0]['id'];
-      print('temperature: $temperature');
-      print('city: $city');
-      print('weatherId: $weatherId');
-    } else {
-      print(response.statusCode);
-    }
+    getLocationData();
   }
 
   @override
   Widget build(BuildContext context) {
-    getData();
     return Scaffold();
   }
 }
--- lib/services/location.dart
+++ lib/services/location.dart
@@ -1,13 +1,16 @@
 import 'package:geolocator/geolocator.dart';
 
+var openWeatherMapApiKey = 'e542f831aca4693ae9887bddb0efe71d';
+
 class Location {
   double latitude;
   double longitude;
 
-  Future<void> getLocation() async {
+  Future getCurrentLocation() async {
     try {
       Position position = await Geolocator().getCurrentPosition(
-        desiredAccuracy: LocationAccuracy.high,);
+        desiredAccuracy: LocationAccuracy.high,
+      );
       latitude = position.latitude;
       longitude = position.longitude;
     } catch (e) {
